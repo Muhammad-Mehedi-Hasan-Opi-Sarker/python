1f47ad104844cae199742a7f2ae9883758bfdbb9
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-07-03 23:02:38 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+price = 400;
+if price < 500:
+    print('i will buy the Samsung Pho9ne')
+else:
+    print('I will be happy with the LG')
---
+++ opi.py
@@ -0,0 +1,9 @@
+age = 45;
+if age<5:
+    print('you are a mad');
+elif age>90:
+    print('tumi ekta chagol');
+elif age<23:
+    print('beshi kore haso')
+else:
+    print('tomake haste hobe na')
